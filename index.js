4e4c185fb6a453b474212905b16c7c5c5a342572
Jordan Alvarado <jordan.alvarado@me.com>
2021-03-02 12:40:24 -0500

    made change regarding input error

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
+//Try to answer the comment that Myra left in my PR regarding error checking before running an actual input
 
 const diceImg = {
-    1: '&#9856',
-    2: '&#9857',
-    3: '&#9858',
-    4: '&#9859',
-    5: '&#9860',
-    6: '&#9861'
-}
-let inputHistory = 0
-let oldRolls = []
+  1: "&#9856",
+  2: "&#9857",
+  3: "&#9858",
+  4: "&#9859",
+  5: "&#9860",
+  6: "&#9861",
+};
+let inputHistory = 0;
+let oldRolls = [];
 
-function rollDice(){
-    let invalid = document.getElementById('invalid')
-    let diceTotal = document.getElementById('dice-para')
-    let sum = document.getElementById('sum-para')
-    let ul = document.querySelector('#roll-history')
-    let input = Number(document.getElementById('input').value)
+function rollDice() {
+  let invalid = document.getElementById("invalid");
+  let diceTotal = document.getElementById("dice-para");
+  let sum = document.getElementById("sum-para");
+  let ul = document.querySelector("#roll-history");
+  let input = Number(document.getElementById("input").value);
 
-    if(input > 0){
-        invalid.textContent = ""
-        diceTotal.innerHTML = ""
-        let sumDice = 0
-        
-        for(let i = 0; i < input; i++){
-            let diceRolled = Math.floor((Math.random()*6)+ 1)
-            diceTotal.innerHTML += diceImg[diceRolled]
-            sumDice += diceRolled
-        }
-        inputHistory++
-        sum.innerText = "Sum = " + sumDice
-        oldRolls[inputHistory] = diceTotal.innerHTML + " = " + sumDice
+  if (input < 1) {
+    invalid.textContent = "Please enter a number greater than 0";
+    diceTotal.innerHTML = "";
+    return;
+  }
 
-        if(inputHistory > 1){
-            let newList = document.createElement("li")
-            newList.textContent = oldRolls[inputHistory -1]
-            ul.appendChild(newList)
-        }
-    }
+  invalid.textContent = "";
+  diceTotal.innerHTML = "";
+  let sumDice = 0;
 
-    else{
-        invalid.textContent = "Please enter a number greater than 0"
-        diceTotal.innerHTML = ""
-    }
+  for (let i = 0; i < input; i++) {
+    let diceRolled = Math.floor(Math.random() * 6 + 1);
+    diceTotal.innerHTML += diceImg[diceRolled];
+    sumDice += diceRolled;
+  }
+  inputHistory++;
+  sum.innerText = "Sum = " + sumDice;
+  oldRolls[inputHistory] = diceTotal.innerHTML + " = " + sumDice;
+
+  if (inputHistory > 1) {
+    let newList = document.createElement("li");
+    newList.textContent = oldRolls[inputHistory - 1];
+    ul.appendChild(newList);
+  }
 }
